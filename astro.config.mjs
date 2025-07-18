039772ec50dc94d57662d694c60e755f4a5b107c
TyraelWasTaken <samuelmilesadcock@hotmail.com>
2025-07-19 00:11:34 +0100

    update site

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import icon from "astro-icon"; // https://www.astroicon.dev/guides/upgrade/v1/
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://aqualanddivers.netlify.app/",
 	integrations: [
 		// example auto import component into blog post mdx files
 		AutoImport({
