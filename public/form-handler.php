b74331ceb26e2d360ef52c730cbb37594c1a7628
TyraelWasTaken <samuelmilesadcock@hotmail.com>
2026-02-17 10:39:41 +0000

    Update thank you message and form handler

--- public/form-handler.php
+++ public/form-handler.php
@@ -12,7 +12,7 @@ $message = htmlspecialchars($_POST["message"]);
 $to      = "contact@twttesting.online";
 $subject = "New Contact Form Message";
 
-$body = "You received a new message:\n\n";
+$body = "You have received a new message:\n\n";
 $body .= "Name: $name\n";
 $body .= "Email: $email\n\n";
 $body .= "Message:\n$message\n";
@@ -22,6 +22,18 @@ $headers .= "Reply-To: $email\r\n";
 $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
 if (mail($to, $subject, $body, $headers)) {
+  $replySubject = "Thanks for contacting Aqualand Divers!";
+
+  $replyBody = "Dear $name,\n\n";
+  $replyBody .= "Thank you for reaching out to Aqualand Divers, we have received your message and will get back in touch shortly.\nUntil then, you can call us any time at 01245 477701\n";
+  $replyBody .= "Kind regards,\n";
+  $replyBody .= "Aqualand Divers Team\n";
+  $replyBody .= "https://twttesting.online\n";
+
+  $replyHeaders = "From: Aqualand Divers <contact@twttesting.online>\r\n";
+  $replyHeaders .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+  mail($email, $replySubject, $replyBody, $replyHeaders);
   header("Location: /thank-you");
   exit();
 } else {
