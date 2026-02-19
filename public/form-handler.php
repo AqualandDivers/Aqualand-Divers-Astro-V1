59d40021319051bfb0e9f5b2f6926a41cc5e90ef
AqualandDivers <AqualandDiversServices@gmail.com>
2026-02-19 10:24:08 +0000

    Update to new website

--- public/form-handler.php
+++ public/form-handler.php
@@ -4,7 +4,7 @@ $name    = htmlspecialchars($_POST["name"]);
 $email   = htmlspecialchars($_POST["email"]);
 $message = htmlspecialchars($_POST["message"]);
 
-$to      = "contact@twttesting.online";
+$to      = "contact@aqualanddivers.co.uk";
 $subject = "New Contact Form Message";
 
 $body = "You have received a new message:\n\n";
@@ -12,7 +12,7 @@ $body .= "Name: $name\n";
 $body .= "Email: $email\n\n";
 $body .= "Message:\n$message\n";
 
-$headers = "From: contact@twttesting.online\r\n";
+$headers = "From: contact@aqualanddivers.co.uk\r\n";
 $headers .= "Reply-To: $email\r\n";
 $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
@@ -23,9 +23,9 @@ if (mail($to, $subject, $body, $headers)) {
   $replyBody .= "Thank you for reaching out to Aqualand Divers, we have received your message and will get back in touch shortly.\nUntil then, you can call us any time at 01245 477701.\n\n";
   $replyBody .= "Kind regards,\n";
   $replyBody .= "Aqualand Divers Team\n";
-  $replyBody .= "https://twttesting.online\n";
+  $replyBody .= "https://aqualanddivers.co.uk\n";
 
-  $replyHeaders = "From: Aqualand Divers <contact@twttesting.online>\r\n";
+  $replyHeaders = "From: Aqualand Divers <contact@aqualanddivers.co.uk>\r\n";
   $replyHeaders .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
   mail($email, $replySubject, $replyBody, $replyHeaders);
